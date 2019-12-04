ef817a36e8f7070dc173e8e00bd6721fcc1735ee
dee <57253652+Angelus81@users.noreply.github.com>
2019-12-04 19:30:12 +0200

    notes added.

--- notes.js
+++ notes.js
@@ -18,3 +18,15 @@
 //       <p>{this.props.greeting}</p>
 // </div>
 // // sasdasas
+
+// **********************************************
+// Uploading to Github from desktop:
+// cd/your project directory
+
+//  1) git init
+//  2) git add . or git add ['filename']
+//  3) git commit -m "My first File"
+//  4) git remote add origin https://github.com/yourusername/your-repo-name.git
+//  5) git pull origin master
+//  6) git push origin master
+// ***********************************************

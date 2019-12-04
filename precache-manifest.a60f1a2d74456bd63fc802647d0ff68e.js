1644725311c860629f0170d01bb7bfd6f7e711b9
dee <57253652+Angelus81@users.noreply.github.com>
2019-12-04 20:47:50 +0200

    Updates

---
+++ precache-manifest.a60f1a2d74456bd63fc802647d0ff68e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0497fcfab089c24eb30e3dd11e02040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3d93030ad2d1c94313d9",
+    "url": "/static/css/main.95fcd9a1.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "3d93030ad2d1c94313d9",
+    "url": "/static/js/main.5b6ad161.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);

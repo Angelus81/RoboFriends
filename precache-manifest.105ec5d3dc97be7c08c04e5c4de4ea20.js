c4bd8a86479073a128540fbb376c05fe7b176de7
dee <57253652+Angelus81@users.noreply.github.com>
2019-12-04 21:50:04 +0200

    Updates

---
+++ precache-manifest.105ec5d3dc97be7c08c04e5c4de4ea20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cb830dec40e4d50690bd4fb593e30b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "20664344eeb1fffc21a6",
+    "url": "/robofriends/static/css/main.fc30bd37.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "20664344eeb1fffc21a6",
+    "url": "/robofriends/static/js/main.51bf8bfc.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
